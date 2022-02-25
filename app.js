61fadd5fd9a04d52045fa18479c54715acae25ea
unknown <shota.inoue@vibin.tokyo>
2022-02-25 22:37:57 +0900

    yeet

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const app = express();
 require("dotenv").config();
 
 const run = () => {
-  getData().then(async (data) => {
-    for (let i = 0; i < data.length; i++) {
-      if (!data[i].email || !data[i].plan) {
-        updatePage(data[i].data.id, "情報不足");
-      } else {
-        const name = data[i].name
-        const email = data[i].email
-        const page_id = data[i].data.id
-        const temp = await getTemp(data[i].plan);
-        const text = `${name}<br>${temp.text}`;
-        sendEmail(email, temp.title, text)
-          .then(updatePage(page_id, "連絡済み"))
-      }
-    }
-  });
+    getData().then(async(data) => {
+        for (let i = 0; i < data.length; i++) {
+            if (!data[i].email || !data[i].plan) {
+                updatePage(data[i].data.id, "情報不足");
+            } else {
+                const name = data[i].name
+                const email = data[i].email
+                const page_id = data[i].data.id
+                const temp = await getTemp(data[i].plan);
+                const text = `${name}<br>${temp.text}`;
+                sendEmail(email, temp.title, text)
+                    .then(updatePage(page_id, "連絡済み"))
+            }
+        }
+    });
 };
 
 run();
